78d3a5932b0e6e3cf8b34cc61605cf6d7f95a93c
k3mil <kamilszkolka123456789@gmail.com>
2026-09-25 12:12:51 +0200

    interfejs do pobierania pytan

--- app/src/main/java/com/example/jsonpopek/MainActivity.java
+++ app/src/main/java/com/example/jsonpopek/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jsonpopek;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Pytanie> pytanie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://my-json-server.typicode.com/k3mil/json-publicServer")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+    Call<ArrayList<Pytanie>> call = jsonPlaceHolderApi.getPytania();
+    call.enqueue(
+            new Callback<ArrayList<Pytanie>>() {
+                @Override
+                public void onResponse(Call<ArrayList<Pytanie>> call, Response<ArrayList<Pytanie>> response) {
+                    if (!response.isSuccessful()){
+                        Toast.makeText(MainActivity.this, pytanie.get(0).trescPytania, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<Pytanie>> call, Throwable t) {
+
+                }
+            }
+    );
+}
 }
